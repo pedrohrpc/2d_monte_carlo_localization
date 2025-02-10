98022f4a63efb47d79f8fd5fba169778687c537a
Pedro Procopio <pedrohrpcorrea@gmail.com>
2025-02-10 11:18:43 -0300

    first commit: simulation working, using 100 particles and 30 fps (1 pf exec/s), not showing particles, not moving particles, not resampling

---
+++ fov_utils.py
@@ -0,0 +1,70 @@
+import cv2 as cv
+import numpy as np
+
+class fov_utils:
+
+    # Gets the trapezoid that limits the robot FOV
+    def get_fov_trapezoid(robot_angle, fov_angle, min_range, max_range):
+
+        x, y = (max_range, max_range)
+        vertice_1 = (int(x+min_range*np.cos(np.radians(robot_angle+fov_angle/2))), int(y-min_range*np.sin(np.radians(robot_angle+fov_angle/2))))
+        vertice_2 = (int(x+max_range*np.cos(np.radians(robot_angle+fov_angle/2))), int(y-max_range*np.sin(np.radians(robot_angle+fov_angle/2))))
+        vertice_3 = (int(x+max_range*np.cos(np.radians(robot_angle-fov_angle/2))), int(y-max_range*np.sin(np.radians(robot_angle-fov_angle/2))))
+        vertice_4 = (int(x+min_range*np.cos(np.radians(robot_angle-fov_angle/2))), int(y-min_range*np.sin(np.radians(robot_angle-fov_angle/2))))
+        
+
+        return np.array([vertice_1, vertice_2, vertice_3, vertice_4])
+
+    # Crops said FOV from the field to represent player's POV and returns it as a cv2 image
+    def get_fov(field, robot_pos, range, fov_area):
+        border = 2*range
+        field_contrast = cv.addWeighted(field, 2, np.zeros(field.shape, field.dtype), 0, -100) 
+        expanded_field = cv.copyMakeBorder(field_contrast, top=border, bottom=border, left=border, right=border, borderType=cv.BORDER_CONSTANT, value=[0, 0, 0])
+        x, y = robot_pos
+        x+= border
+        y+= border
+        cropped_field = expanded_field[(int(y)-range):(int(y)+range),(int(x)-range):(int(x)+range)]
+        mask = np.zeros_like(cropped_field)
+        # mask = cv.circle(mask, (range,range), range, (255,255,255), -1)
+        cv.fillConvexPoly(mask, fov_area, (255,255,255))
+        robot_fov = cv.bitwise_and(cropped_field,mask)
+        
+        return robot_fov
+
+    # Used to rotate an cv2 image, in this case the player's POV for better perspective
+    def cv_rotate(image, angle, center = None, scale = 1.0):
+        (h, w) = image.shape[:2]
+
+        if center is None:
+            center = (w / 2, h / 2)
+
+        # Perform the rotation
+        M = cv.getRotationMatrix2D(center, angle, scale)
+        rotated = cv.warpAffine(image, M, (w, h))
+
+        return rotated
+
+    # Reduces the resolution of the FOV to make it lighter
+    def to_low_res(image,compress_ratio):
+        (h, w) = image.shape[:2]
+        gray_image = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+        blured_image = cv.GaussianBlur(gray_image, (compress_ratio*4+1,compress_ratio*4+1), 0)
+        # blured_image = (gray_image)
+        compressed_image = cv.resize(blured_image, (int(w/compress_ratio), int(h/compress_ratio)))
+        compressed_image = compressed_image/255
+        return compressed_image
+    
+    
+    def get_particle_pov(field_cv, player_pos, player_angle, fov_angle, fov_min_range, fov_max_range, low_res = True):
+        # Calc the are that will be cropped
+        fov_area = fov_utils.get_fov_trapezoid(player_angle, fov_angle, fov_min_range, fov_max_range)
+        # Get the fov (area)
+        robot_fov = fov_utils.get_fov(field_cv, player_pos, fov_max_range, fov_area)
+        # Rotate fov for comparrison
+        pov = fov_utils.cv_rotate(robot_fov, 90-player_angle)[int(fov_max_range*(1-np.sin(np.radians(fov_angle/2)))):fov_max_range-fov_min_range,int(fov_max_range*(1-np.cos(np.radians(fov_angle/2)))):int(fov_max_range*(1+np.cos(np.radians(fov_angle/2))))]
+
+        # Using low resolution for particle comparison
+        if low_res: pov = fov_utils.to_low_res(pov,compress_ratio=5)
+        
+        return pov
+    
---
+++ main.py
@@ -0,0 +1,230 @@
+# Example file showing a circle moving on screen
+import pygame
+import numpy as np
+import cv2 as cv
+import threading
+from fov_utils import fov_utils as fov
+from ParticleFilter import ParticleFilter as pf
+
+
+class simulation:
+    def __init__(self, fov_angle, fov_min_range, fov_max_range, visual_particles = True, visual_fov = True, fps = 60):
+        # pygame setup
+        pygame.init()
+        self.field = pygame.image.load("2d_localization/images/soccer_field.jpg")
+        self.field_w, self.field_h = self.field.get_size()
+        self.screen = pygame.display.set_mode((self.field_w, self.field_h))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.fps = fps
+
+        # Parameters
+        self.dt = 0
+        self.base_speed_esc = 50
+        self.forward_speed_esc = self.base_speed_esc
+        self.speed_ang = 50
+        self.acceleration_esc = 50
+        self.acceleration_ang = 50
+        self.player = pygame.image.load("2d_localization/images/robot.png")
+        self.player = pygame.transform.rotate(self.player, 90)
+        self.player_w, self.player_h = self.player.get_size()
+
+        self.particle_icon = pygame.image.load("2d_localization/images/particle.png")
+        self.particle_icon_w, self.particle_icon_h = self.particle_icon.get_size()
+
+        self.player_angle = 0
+        self.field_center_pos = pygame.Vector2(self.field_w/2, self.field_h/2)
+        self.player_pos = pygame.Vector2(self.field_center_pos)
+
+
+        ### OPENCV FOV PARAMETERS ###
+        self.fov_angle = fov_angle #degrees
+        self.fov_min_range = fov_min_range #centimeters
+        self.fov_max_range = fov_max_range #centimeters
+        self.field_cv = cv.imread("2d_localization/images/soccer_field.jpg")
+        self.particles_visual_ready = False
+
+        ## Visual features (for debugging)
+        self.visual_particles = visual_particles
+        self.visual_fov = visual_fov
+
+    # Used to rotate a image, in this case the player
+    def blitRotate(surf, image, pos, originPos, angle):
+
+        # offset from pivot to center
+        image_rect = image.get_rect(topleft = (pos[0] - originPos[0], pos[1]-originPos[1]))
+        offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
+        
+        # roatated offset from pivot to center
+        rotated_offset = offset_center_to_pivot.rotate(-angle)
+
+        # roatetd image center
+        rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
+
+        # get a rotated image
+        rotated_image = pygame.transform.rotate(image, angle)
+        rotated_image_rect = rotated_image.get_rect(center = rotated_image_center)
+
+        # rotate and blit the image
+        surf.blit(rotated_image, rotated_image_rect)
+
+    def update_sim(self):
+        self.screen.blit(self.field, (0,0))
+        #The player walking in the field
+        simulation.blitRotate(self.screen, self.player, self.player_pos,(self.player_w/2, self.player_h/2), self.player_angle) 
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.player_pos.y -= np.sin(np.radians(self.player_angle)) * self.forward_speed_esc * self.dt
+            self.player_pos.x += np.cos(np.radians(self.player_angle)) * self.forward_speed_esc * self.dt
+
+        if keys[pygame.K_s]:
+            self.player_pos.y += np.sin(np.radians(self.player_angle)) * self.base_speed_esc * self.dt
+            self.player_pos.x -= np.cos(np.radians(self.player_angle)) * self.base_speed_esc * self.dt
+
+        if keys[pygame.K_a]:
+            self.player_pos.y -= np.sin(np.radians(self.player_angle+90)) * self.base_speed_esc * self.dt
+            self.player_pos.x += np.cos(np.radians(self.player_angle+90)) * self.base_speed_esc * self.dt
+
+        if keys[pygame.K_d]:
+            self.player_pos.y += np.sin(np.radians(self.player_angle+90)) * self.base_speed_esc * self.dt
+            self.player_pos.x -= np.cos(np.radians(self.player_angle+90)) * self.base_speed_esc * self.dt
+
+        if keys[pygame.K_q]:
+            self.player_angle += self.speed_ang * self.dt
+        if keys[pygame.K_e]:
+            self.player_angle -= self.speed_ang * self.dt
+
+
+        # adding angular acceleration
+        if (keys[pygame.K_q] or keys[pygame.K_e]) and self.speed_ang < 150:
+            self.speed_ang += self.acceleration_ang * self.dt
+        if (keys[pygame.K_q] == False and keys[pygame.K_e] == False):
+            self.speed_ang = 50
+
+        # adding escalar acceleration (forward)
+        if keys[pygame.K_w] and self.forward_speed_esc < 150:
+            self.forward_speed_esc += self.acceleration_esc * self.dt
+        if keys[pygame.K_w] == False:
+            self.forward_speed_esc = self.base_speed_esc
+
+        if keys[pygame.K_SPACE]:
+            self.player_pos = pygame.Vector2(self.field_center_pos)
+            self.player_angle = 0
+
+        if keys[pygame.K_ESCAPE]:
+            self.running == False
+
+        pygame.display.flip()
+
+        # limits FPS to 60
+        self.dt = self.clock.tick(self.fps) / 1000
+
+    def robot_fov(self):
+        # Calc the are that will be cropped
+        robot_pov = fov.get_particle_pov(self.field_cv, self.player_pos, self.player_angle, self.fov_angle, self.fov_min_range, self.fov_max_range, low_res = False)
+
+        robot_pov_low_res = fov.get_particle_pov(self.field_cv, self.player_pos, self.player_angle, self.fov_angle, self.fov_min_range, self.fov_max_range, low_res = True)
+        
+        if self.visual_fov:
+            cv.imshow("Robot Field of View", robot_pov)
+            cv.imshow("Robot Field of View Low Resolution", robot_pov_low_res)
+        cv.waitKey(1)
+
+        return robot_pov_low_res
+    
+    def particle_result(self, particle_x, particle_y, particle_angle,robot_fov):
+        particle_pov = fov.get_particle_pov(self.field_cv, pygame.Vector2(particle_x, particle_y), particle_angle, self.fov_angle, self.fov_min_range, self.fov_max_range, low_res = True)
+        diff = (1-(np.sum(np.abs(np.subtract(robot_fov,particle_pov))))/(robot_fov.shape[0]*robot_fov.shape[1]/(180/self.fov_angle)))**2
+        diff_percent = diff*100
+
+        return particle_pov, diff_percent
+
+    def run_particle_filter():
+        pass
+
+    def update_particles_visual(self, robot_fov, particleFilter: pf):
+        aux_count = 1
+        weights = []
+        # print('particles: ')
+        if self.visual_particles:
+            horizontal_strip = np.ones((int(robot_fov.shape[0]/10),robot_fov.shape[1]), robot_fov.dtype)
+            l_concat = horizontal_strip.copy()
+            r_concat = horizontal_strip.copy()
+        
+        for particle in particleFilter.particles:
+            particle_pov, diff_percent = self.particle_result(particle[0], particle[1], particle[2],robot_fov)
+
+            if self.visual_particles:
+                print(f'partice {aux_count}:\nsum: {np.sum(particle_pov)}\ndiff: {diff_percent}%')
+                if (aux_count<=N/2):
+                    l_concat = np.concatenate((l_concat,particle_pov,horizontal_strip), axis=0)
+                else:
+                    r_concat = np.concatenate((r_concat,particle_pov,horizontal_strip), axis=0)
+            aux_count += 1
+            
+            weights.append(diff_percent/100)
+
+        if self.visual_particles:
+            vertical_strip = np.ones((l_concat.shape[0],int(l_concat.shape[1]/10)), robot_fov.dtype)
+            concat_result = np.concatenate((l_concat,vertical_strip,r_concat), axis=1)
+            self.particles_visual = concat_result
+            self.particles_visual_ready = True
+
+        particleFilter.weights = weights
+        particleFilter.weights = np.divide(particleFilter.weights,sum(particleFilter.weights))
+
+if __name__ == '__main__':
+    ### Simulation params
+    exec_freq = 1 # times per second
+    aux_count = 0
+
+    ### Real robot params
+    fov_max_range = 500 #centimeters
+    fov_min_range = 50 #centimeters
+    fov_angle = 90 #degrees
+
+    ### Particle filter params
+    N = 100
+    # camHeight = 80
+    # camAngle = np.pi/4
+    # fov = (3/3) * np.pi
+    # minRange = camAngle * np.tan(camAngle-fov/2)
+    # maxRange = camAngle * np.tan(camAngle+fov/4)
+
+    sim = simulation(fov_angle, fov_min_range, fov_max_range, visual_particles=False, visual_fov=False, fps = 30)
+    particleFilter = pf(N,fov_max_range,fov_min_range,fov_max_range,
+                        previousPositionKnown=False, xRange = [0, sim.field_w], yRange = [0, sim.field_h], headingRange = [0, 360])
+
+
+
+    while sim.running:
+        # pygame.QUIT event means the user clicked X to close your window
+        pygame.display.set_caption(f'Robot position - X: {int(sim.player_pos[0])} Y: {int(sim.player_pos[0])}')
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sim.running = False
+
+        sim.update_sim()
+        robot_fov = sim.robot_fov()
+        particle_update_thread = threading.Thread(target=sim.update_particles_visual, args=(robot_fov, particleFilter), daemon=True)
+        # print(robot_fov.max())
+        # print(robot_fov.shape[0]*robot_fov.shape[1])
+
+
+
+        if aux_count == sim.fps/exec_freq:
+            particle_update_thread.start()
+            print(particleFilter.weights)
+            print(f'Neff: {particleFilter.neff()}')
+            print(f'N/2: {N/2}')
+            aux_count = 0
+        
+        if sim.particles_visual_ready and sim.visual_particles:
+            cv.imshow(f'Particles', sim.particles_visual)
+        cv.waitKey(1)
+        
+        aux_count += 1
+
+    pygame.quit()
